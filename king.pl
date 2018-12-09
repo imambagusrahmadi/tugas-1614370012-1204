a544cd7cf420e60318e2fc49ca606285f76dee3f
imambagusrahmadi <45400849+imambagusrahmadi@users.noreply.github.com>
2018-12-10 00:59:16 +0700

    Add files via upload

---
+++ king.pl
@@ -0,0 +1,11 @@
+pria(x,bahrun).
+pria(bahrun,fahri).
+perempuan(fahri,salsa).
+pria(fahri,arfan).
+perempuan(bahrun,salma).
+pria(salma,farhan).
+perempuan(x,farah).
+pria(farah,ferdi).
+perempuan(ferdi,syifa).
+pria(ferdi,faiz).
+pria(farah,burhan).
